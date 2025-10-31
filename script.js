fb23cb002b30867d1983586ff6835be59da6d27d
Sunny <91402650+SunnyYeahBoiii@users.noreply.github.com>
2025-10-31 23:05:36 +0700

    update appearance

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const unitBoard = document.getElementById("unit-board");
 const unitBtn = document.getElementById("unit-btn");
+const dayOfTheWeekBoard = document.getElementById("day-of-the-week-board")
+const dayOfTheWeekBtn = document.getElementById("forecast-day-btn")
 
 unitBtn.addEventListener("click" , function(event){
     event.stopPropagation();
     unitBoard.classList.toggle("hidden");
-})
+});
 
 unitBoard.addEventListener("click" , function(event){
     event.stopPropagation();
-})
+});
+
+dayOfTheWeekBtn.addEventListener("click" , function(event){
+    event.stopPropagation();
+    dayOfTheWeekBoard.classList.toggle("hidden");
+});
+
+dayOfTheWeekBoard.addEventListener("click" , function(event){
+    event.stopPropagation();
+});
